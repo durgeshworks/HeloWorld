76f74aa741c0016d77f82c9a5b88531791a999b3
durgeshworks <durgeshanju@gmail.com>
2017-05-05 10:20:30 +0000

    Fixed deployment scripts

--- DeploymentScripts/downloadpackage.ps1
+++ DeploymentScripts/downloadpackage.ps1
@@ -1,13 +1,9 @@
-# Your account access key - must have read access to your S3 Bucket
-$accessKey = "YOUR-ACCESS-KEY"
-# Your account secret access key
-$secretKey = "YOUR-SECRET-KEY"
 # The region associated with your bucket e.g. eu-west-1, us-east-1 etc. (see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-regions)
-$region = "eu-west-1"
+$region = "us-east-1"
 # The name of your S3 Bucket
-$bucket = "my-test-bucket"
+$bucket = "anjaneyademo"
 # The folder in your bucket to copy, including trailing slash. Leave blank to copy the entire bucket
-$keyPrefix = "my-folder/"
+$keyPrefix = ""
 # The local file path where files should be copied
 $localPath = "C:\"
 
